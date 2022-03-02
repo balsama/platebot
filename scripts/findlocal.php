96e97bdfb5dd647204350a5c92085ed34fcc540d
Adam Balsam <balsama@users.noreply.github.com>
2022-03-01 23:29:50 -0500

    Reformat responses (#1)

---
+++ scripts/findlocal.php
@@ -0,0 +1,6 @@
+<?php
+include_once(__DIR__ . '/../vendor/autoload.php');
+
+use Balsama\Bostonplatebot\Find;
+
+$find = new Find('localhost/boston-plate-lookup');
--- src/Reply.php
+++ src/Reply.php
@@ -10,34 +10,90 @@ class Reply
 {
 
     private BirdElephant $twitter;
-    private string $fullTweetText;
-    private array $tweetTexts;
+    private string $triggerTweetId;
 
-    public function __construct(BiElTweet $triggerTweet, string $fullTweetText)
+    public function __construct(BiElTweet $triggerTweet, $plateInfo)
     {
         $this->twitter = Initialize::initialize();
-        $this->fullTweetText = $fullTweetText;
-        $this->tweetTexts = str_split($this->fullTweetText, 230);
-        $initialTweet = $this->tweetInitialTweet($triggerTweet->id);
+        $this->triggerTweetId = $triggerTweet->id;
 
-        if (count($this->tweetTexts) > 1) {
-            $this->sendReplies($initialTweet);
+        $this->chunkTweets($plateInfo);
+    }
+
+    private function chunkTweets(\stdClass $plateInfo): void
+    {
+        $tweets = [];
+        $tweets[] = $this->generateFirstTweet($plateInfo);
+        if ($plateInfo->tickets) {
+            $ticketsTweets = $this->ticketsToTweetStrings($plateInfo->tickets);
+            $tweets = array_merge($tweets, $ticketsTweets);
         }
-    }
 
-    private function tweetInitialTweet($triggerTweetId)
-    {
-        return $this->postReply($triggerTweetId, $this->tweetTexts[0]);
-    }
-
-    private function sendReplies($tweetToReplyTo)
-    {
-        $replies = $this->tweetTexts;
-        array_shift($replies);
-        foreach ($replies as $reply) {
-            $tweet = $this->postReply($tweetToReplyTo->data->id, $reply);
+        $tweetToReplyToId = $this->triggerTweetId;
+        foreach ($tweets as $tweet) {
+            $tweet = $this->postReply($tweetToReplyToId, $tweet);
             $tweetToReplyTo = $tweet;
+            $tweetToReplyToId = $tweetToReplyTo->data->id;
         }
+    }
+
+    private function ticketsToTweetStrings(array $tickets): array
+    {
+        $tickets = array_reverse($tickets);
+        $formattedTickets = [];
+        foreach ($tickets as $ticket) {
+            $formattedTickets[] = $this->ticketToString($ticket);
+        }
+        $i = 0;
+        $n = 0;
+        $chunkedFormattedTickets = [];
+        foreach ($formattedTickets as $formattedTicket) {
+            $chunkedFormattedTickets[$n][$i] = $formattedTicket;
+            $i++;
+            if ($i == 3) {
+                $chunkedFormattedTickets[$n] = implode(PHP_EOL, $chunkedFormattedTickets[$n]);
+                $i = 0;
+                $n++;
+            }
+        }
+        if (is_array($chunkedFormattedTickets[$n])) {
+            $chunkedFormattedTickets[$n] = implode(PHP_EOL, $chunkedFormattedTickets[$n]);
+        }
+
+        return $chunkedFormattedTickets;
+    }
+
+    private function ticketToString(\stdClass $ticket):string
+    {
+        $format = '- %s: $%d, %s %s at %s.';
+        return sprintf($format, $ticket->infraction, $ticket->fine, $ticket->infraction_date, $ticket->infraction_time, $ticket->infraction_address);
+    }
+
+    private function generateFirstTweet(\stdClass $plateInfo):string
+    {
+        if (!$plateInfo->found) {
+            if ($plateInfo->tickets) {
+                $totalTweets = round(((count($plateInfo->tickets))/3), 0) + 1;
+                $format = 'Unable to find current balance for plate %s in the system, but we did find the following %d tickets associated with that plate.' . PHP_EOL . PHP_EOL . '🧵👇 [1/%d]';
+                $firstTweet = sprintf($format, strtoupper($plateInfo->plate_number), count($plateInfo->tickets), $totalTweets);
+            }
+            else {
+                $format = 'Unable to find current balance for plate %s or any tickets associated with that plate.🤷‍♀️';
+                $firstTweet = sprintf($format, strtoupper($plateInfo->plate_number));
+            }
+        }
+        else {
+            if ($plateInfo->tickets) {
+                $totalTweets = round(((count($plateInfo->tickets))/3), 0) + 1;
+                $format = 'Plate %s has a current balance of $%4.2f and we found %d tickets associated with that plate.' . PHP_EOL . PHP_EOL . '🧵👇 [1/%d]';
+                $firstTweet = sprintf($format, strtoupper($plateInfo->plate_number), $plateInfo->balance, count($plateInfo->tickets), $totalTweets);
+            }
+            else {
+                $format = 'Plate %s has a current balance of $%4.2f and we found zero tickets associated with that plate.';
+                $firstTweet = sprintf($format, strtoupper($plateInfo->plate_number), $plateInfo->balance);
+            }
+        }
+        return $firstTweet;
     }
 
     private function postReply($tweetToReplyToId, $replyText)
